e8e038877903d0e647ab2b5c7fdbaf8473f32ba6
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2022-03-28 15:01:20 -0700

    added new tests and req

--- cohort/forum/models.py
+++ cohort/forum/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Topic(models.Model):
     title = models.CharField(max_length=200)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
